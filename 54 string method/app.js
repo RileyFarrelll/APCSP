e5bba33d9aa4ff8f348de20d4d2125754e55071c
Riley Farrell <rileyfarrell@ORRHS.ORG>
2020-02-27 12:34:34 -0500

    enter

---
+++ 54 string method/app.js
@@ -0,0 +1,23 @@
+let s
+let string = 'my name is riley'       
+//check length
+console.log(string.length)
+//conversion
+let num = 10
+num.toString()
+console.log(typeof num)
+//searching
+s =string.search('r')
+console.log(s)
+s = string.startsWith('i')
+s = string.includes('x')
+//modify
+s = string.toLowerCase()
+s = string.toUpperCase()
+s = string.concat('hello')
+console.log(s)
+s = string.slice(3,8)
+s = string.trim()
+console.log()
+num = Number(num)
+console.log(typeof num)
