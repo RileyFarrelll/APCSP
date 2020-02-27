83fd58fa796351383a699771b54ed58dffdc783e
Riley Farrell <rileyfarrell@ORRHS.ORG>
2020-02-27 11:55:00 -0500

    changes

---
+++ clicker game v2/start.js
@@ -0,0 +1,9 @@
+function setup(){
+createCanvas(windowWidth, windowHeight)
+const heading=createElement('h', 'welcome to catch the banana')
+heading.style('text-align: center')
+heading.style('position: absolute')
+
+
+}
+
